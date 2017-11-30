82a431f631f9cd7f3812ef9addbe5b9b5a1c0117
Shawn Baugh II <shawnbaughcreative@gmail.com>
2017-11-29 21:03:52 -0600

    Completed 6th application. Starting 7th.

--- firebaseauth/App.js
+++ firebaseauth/App.js
@@ -1,11 +1,28 @@
+import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase';
+import SignUpForm from './components/SignUpForm';
+import SignInForm from './components/SignInForm';
 
 export default class App extends React.Component {
+    componentDidMount() {
+    const config = {
+        apiKey: "AIzaSyAMM1Uffh1wMbPd2u6j6LWQ8dzVnO8k2cw",
+        authDomain: "one-time-password-d12ad.firebaseapp.com",
+        databaseURL: "https://one-time-password-d12ad.firebaseio.com",
+        projectId: "one-time-password-d12ad",
+        storageBucket: "one-time-password-d12ad.appspot.com",
+        messagingSenderId: "835343794617"
+        };
+        firebase.initializeApp(config);
+    }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <SignUpForm />
+        <SignInForm />
       </View>
     );
   }
@@ -16,6 +33,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
 });
